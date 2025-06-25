75a306b45d964c8721431e4f68d27ec7e5e54364
Gelanye22 <gelan.ye11@gmail.com>
2025-06-25 18:45:30 +0200

    update

--- program/data_imputed.R
+++ program/data_imputed.R
@@ -1,7 +1,7 @@
 data_clean <- readRDS("data/fi_clean.rds")
 train_data_clean <- data_clean %>% filter(dataset == "train")
 test_data_clean <- data_clean %>% filter(dataset == "test")
-  
+
 #select cols for training, use region_code
 train_data_clean  <- train_data_clean %>%
   select(-longitude, -latitude, -lga, -ward, -subvillage, -district_code, -region,-dataset, -region_code)
@@ -29,14 +29,25 @@ train_data_imputed <- train_data_clean %>%
     gps_height = replace_na(gps_height, median(gps_height, na.rm = TRUE)),
     across(where(is.character), as.factor))
 
-#save the imputed dataset
-factor_cols <- names(train_data_imputed)[vapply(train_data_imputed, is.factor, logical(1))]
+# Select rows with missing latitude or longitude (test data)
 test_data_clean <- test_data_clean %>% 
-  mutate(across(all_of(factor_cols), as.factor)) %>%
+  filter(
+    latitude  ==  0      | latitude  == -2e-08 |
+      longitude ==  0      | longitude == -2e-08
+  )  %>% 
   select(-longitude, -latitude, -lga, -ward, -subvillage,-district_code, -region,-dataset, -region_code)
+
+bool_cols <- c(
+  names(train_data_imputed)[vapply(train_data_imputed, is.logical, logical(1))],
+  names(test_data_clean   )[vapply(test_data_clean,    is.logical, logical(1))]
+)
+
+train_data_imputed <- train_data_imputed %>%
+  mutate(across(all_of(bool_cols), as.logical))
+
+test_data_clean  <- test_data_clean %>%
+  mutate(across(any_of(bool_cols), as.logical)) 
 
 data_imputed <- bind_rows(train_data_imputed, test_data_clean)
 
-saveRDS(train_data_imputed, "data/train_data_imputed.rds")
 saveRDS(data_imputed, "data/data_imputed.rds")
-
