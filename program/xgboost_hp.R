45e55918b19c49f5defaf6154e914e95b4e7eda5
JuHaoran66 <Ju.Haoran@campus.lmu.de>
2025-07-08 10:05:09 +0200

    xg tuning result

--- program/xgboost_hp.R
+++ program/xgboost_hp.R
@@ -9,8 +9,10 @@ library(paradox)
 library(future)
 library(parallel)
 workers <- as.numeric(Sys.getenv("SLURM_CPUS_PER_TASK", unset = "1"))
-plan(multicore, workers = workers)
-fi_clean <- readRDS("data/fi_clean.rds")
+print(workers)
+plan(multisession, workers = 16)
+set.seed(7832)
+fi_clean <- readRDS("data/data_imputed.rds")
 
 # ---- Hyperparameter tuning (region_district and lga)----
 # eta 1e-4, 1  Logscale
@@ -29,8 +31,6 @@ fi_clean <- readRDS("data/fi_clean.rds")
 #
 # subsample 0.1 1
 df_ll <- fi_clean %>%
-  select(-region, -district_code, -subvillage, -ward,
-         -lga, -region_code, -longitude, -latitude) %>%
   select_if(~ length(unique(.[!is.na(.)])) > 1)
 
 train_ll <- df_ll %>% filter(!is.na(status_group))
@@ -52,41 +52,40 @@ graph <- po("colapply",  # char -> factor
   po("encode", method = "treatment") %>>%
   lrn("classif.xgboost",
       predict_type          = "response",
-      nrounds = 500)
+      nrounds = 2,
+      nthread =8)
 
 base_lrn <- GraphLearner$new(graph)
 
-ps <- ps(
+search_space <- paradox::ps(
   "classif.xgboost.eta"             = p_dbl(0.1, 0.2, logscale = TRUE),
-  "classif.xgboost.nrounds"         = p_int(100, 500),
   "classif.xgboost.max_depth"       = p_int(6, 12),
-  "classif.xgboost.subsample"       = p_dbl(0.9, 1),
-  "classif.xgboost.colsample_bytree"= p_dbl(0.9, 1),
+  "classif.xgboost.subsample"       = p_dbl(0.5, 1),
+  "classif.xgboost.colsample_bytree"= p_dbl(0.5, 1),
   "classif.xgboost.lambda"          = p_dbl(0.1, 5, logscale = TRUE),
   "classif.xgboost.alpha"           = p_dbl(0.1, 5, logscale = TRUE)
 )
 
 auto <- AutoTuner$new(
   learner      = base_lrn,
-  resampling   = rsmp("cv", folds = 3),
+  resampling   = rsmp("cv", folds = 5),
   measure      = msr("classif.acc"),
-  search_space = ps,
+  search_space = search_space,
   tuner        = tnr("random_search"),
-  terminator   = trm("evals", n_evals = 30)
+  terminator   = trm("evals", n_evals = 100)
 )
+
+auto$train(task_ll)
 
 rr <- mlr3::resample(task_ll, auto, rsmp("cv", folds = 3))
 acc  <- rr$aggregate(msr("classif.acc"))
 bacc <- rr$aggregate(msr("classif.bacc"))
-
-best_vals <- auto$learner$model$param_set$values
-print(best_vals)
+best_params <- auto$learner$param_set$values
 
 result_list <- list(
-  accuracy          = acc,
-  balanced_accuracy = bacc,
-  best_params       = best_vals
+  acc                = acc,
+  bacc               = bacc,
+  best_params        = best_params
 )
 
-saveRDS(result_list, file = "../result/xgb_tuning_results.rds")
-
+saveRDS(result_list, file = "result/xgb_tuning_results.rds")
