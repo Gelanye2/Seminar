ad6020a84b2ebbee6eceaeaafac6b919608b005e
Gelanye22 <gelan.ye11@gmail.com>
2025-07-12 00:05:47 +0200

    ranger tuning update

--- program/tuning_ranger.R
+++ program/tuning_ranger.R
@@ -4,8 +4,8 @@ library(mlr3pipelines)
 library(mlr3tuning)
 library(paradox)
 library(paradox)
+library(mlr3mbo)
 
-source("setup.R")
 fi_clean <- readRDS("data/fi_clean.rds") # no imp and all
 imputed <- readRDS("data/data_imputed.rds") # all:train + test
 sub_imputed <- readRDS("data/sub_imputed.rds") # train + test without longtitude and latitude
@@ -25,42 +25,73 @@ task_imp <- TaskClassif$new(
   target  = "status_group"
 )
 
+task_imp$col_roles$stratum <- task_imp$target_names
 
-# ---- Pipeline Graph ----
-graph <- 
-  po("colapply", param_vals = list(
-    applicator = function(x) if (is.character(x)) as.factor(x) else x,
-    affect_columns = selector_type("character")
-  )) %>>%
+latlon_to_na <- function(v, thr = 3e-8) {
+  v <- as.numeric(v)
+  v[ abs(v) < thr ] <- NA_real_
+  v
+}
+po_latlon_na <- po("colapply", id = "latlon_to_na",
+                   param_vals = list(
+                     applicator     = latlon_to_na,
+                     affect_columns = selector_name(c("longitude", "latitude"))
+                   ))
+
+po_latlon_flag <- po("mutate", id = "latlon_flag",
+                     param_vals = list(
+                       mutation = list(
+                         longitude_missing = ~ is.na(longitude),
+                         latitude_missing  = ~ is.na(latitude)
+                       )
+                     ))
+
+po_latlon_impute <- po("imputeconstant", id = "latlon_imputer",
+                       param_vals = list(
+                         affect_columns = selector_name(c("longitude", "latitude")),
+                         constant       = -999    
+                       ))
+
+po_char2fac <- po("colapply", id = "char2factor",
+                  param_vals = list(
+                    applicator     = as.factor,
+                    affect_columns = selector_type("character")
+                  ))
+
+graph <- po_latlon_na %>>%
+  po_latlon_flag %>>%
+  po_latlon_impute %>>%
+  po_char2fac %>>%
   po("removeconstants") %>>%
-  lrn("classif.ranger",
-      predict_type = "response"
-  )
+  lrn("classif.ranger", 
+      predict_type = "response")
+  
 
 # GraphLearner
 glrn <- GraphLearner$new(graph)
 
-
 param_set <- ps(
-  classif.ranger.num.trees     = p_int(400, 700),
-  classif.ranger.mtry          = p_int(5, 15),
-  classif.ranger.max.depth     = p_int(20, 60),
-  classif.ranger.min.node.size = p_int(1, 5),
-  classif.ranger.sample.fraction = p_dbl(0.7, 1.0)
+  classif.ranger.num.trees     = p_int(900, 1600),
+  classif.ranger.mtry          = p_int(lower = 3, upper = 7),
+  classif.ranger.max.depth     = p_int(40, 80),
+  classif.ranger.min.node.size = p_int(1, 10),
+  classif.ranger.sample.fraction = p_dbl(0.4, 1.0)
 )
+
+#####
 
 # ---- AutoTuner ----
 at_ranger <- AutoTuner$new(
   learner = glrn,
   resampling = rsmp("cv", folds = 3),
   measure = msr("classif.acc"),
-  terminator = trm("evals", n_evals = 100),
-  tuner = tnr("grid_search", resolution = 5),
+  terminator = trm("evals", n_evals = 20),
+  tuner = tnr("mbo"),
   search_space = param_set
 )
 
 # Trainieren auf Trainingsdaten
-set.seed(42)
+
 at_ranger$train(task_imp)
 
 # Beste Parameter einsehen
