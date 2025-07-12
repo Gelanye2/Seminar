00857e54ea2c7d97dc2123bc7dbdc6ebf8a41584
Gelanye22 <gelan.ye11@gmail.com>
2025-07-12 20:22:25 +0200

    Restore old hyperparameter settings

--- program/xgboost_hp.R
+++ program/xgboost_hp.R
@@ -1,5 +1,5 @@
-source("setup.R")
-
+# source("setup.R")
+.libPaths("/dss/dsshome1/01/ra59qow2/R/x86_64-pc-linux-gnu-library/4.3")
 library(dplyr)
 library(mlr3)          # TaskClassif, msr(), rsmp(), mlr3::resample()
 library(mlr3pipelines) # po(), %>>%, selector_type(), GraphLearner
@@ -8,14 +8,12 @@ library(mlr3tuning)
 library(paradox)
 library(future)
 library(parallel)
-
+library(mlr3mbo)
 library(future.apply)
 workers <- as.numeric(Sys.getenv("SLURM_CPUS_PER_TASK", unset = "1"))
 print(workers)
-
 plan(multisession, workers = 16)
 set.seed(7832)
-
 df <- readRDS("data/data_imputed.rds")
 
 # ---- Hyperparameter tuning (region_district and lga)----
@@ -40,9 +38,6 @@ df_ll <- df %>%
 train_ll <- df_ll %>% filter(!is.na(status_group))
 test_ll  <- df_ll %>% filter( is.na(status_group))
 
-train_ll$status_group <- as.factor(train_ll$status_group)
-test_ll$status_group  <- factor(test_ll$status_group, levels = levels(train_ll$status_group))
-
 # mlr3 task on the training set
 task_ll <- TaskClassif$new(
   id      = "ll",
@@ -50,7 +45,7 @@ task_ll <- TaskClassif$new(
   target  = "status_group"
 )
 
-task_ll$col_roles$stratum <- "status_group"
+task_ll$col_roles$stratum <- task_ll$target_names
 
 latlon_to_na <- function(v, thr = 3e-8) {
   v <- as.numeric(v)
@@ -94,19 +89,11 @@ graph <- po_latlon_na %>>%
       nthread = 7,
       eval_metric = "mlogloss",
       early_stopping_rounds = 10,
-<<<<<<< Updated upstream
       nrounds = to_tune(upper = 500, internal = TRUE),
       eta = to_tune(0.01,0.2, logscale = TRUE),
       max_depth = to_tune(6,14),
       colsample_bytree = to_tune(0.7, 1),
       subsample = to_tune(0.7, 1),
-=======
-      nrounds = to_tune(upper = 100, internal = TRUE),
-      eta = to_tune(0.1,0.3, logscale = TRUE),
-      max_depth = to_tune(3,12),
-      colsample_bytree = to_tune(0.7, 0.9),
-      subsample = to_tune(0.5, 1.0),
->>>>>>> Stashed changes
       lambda = to_tune(1, 5, logscale = TRUE),
       alpha  = to_tune(0.01, 0.5, logscale = TRUE),
       gamma = to_tune(0, 5),
@@ -115,41 +102,20 @@ graph <- po_latlon_na %>>%
 
 base_lrn <- GraphLearner$new(graph)
 
+set_validate(base_lrn, validate = "test", ids = "classif.xgboost")
+
 auto <- AutoTuner$new(
   learner = base_lrn,
   resampling = rsmp("cv", folds = 5),
   measure = msr("classif.acc"),
-<<<<<<< Updated upstream
   tuner = tnr("mbo"),
   terminator = trm("evals", n_evals = 200)
-=======
-  tuner = tnr("random_search"),
-  terminator = trm("evals", n_evals = 50)
->>>>>>> Stashed changes
-)
+  )
 
 auto$train(task_ll)
 
 best_params <- auto$learner$param_set$values
 
-<<<<<<< Updated upstream
 saveRDS(best_params, file = "result/xgb_tuning_params3.rds")
 saveRDS(auto$learner, file = "result/xgb_tuning_model3.rds")
 saveRDS(auto$archive, file = "result/xgb_tuning_archive3.rds")
-=======
-<<<<<<< Updated upstream
-saveRDS(best_params, file = "result/xgb_tuning_params2.rds")
-saveRDS(auto$learner, file = "result/xgb_tuning_model2.rds")
-saveRDS(auto$archive, file = "result/xgb_tuning_archive2.rds")
-
-archive <-readRDS("result/xgb_tuning_archive2.rds")
-dt <- as.data.table(archive)
-top10 <- dt[order(-classif.acc)][1:10]
-top10_params <- top10[, archive$search_space$ids(), with = FALSE]
-
-=======
-saveRDS(best_params, file = "result/xgb_tuning_params.rds")
-saveRDS(auto$learner, file = "result/xgb_tuning_model.rds")
-saveRDS(auto$archive, file = "result/xgb_tuning_archive.rds")
->>>>>>> Stashed changes
->>>>>>> Stashed changes
